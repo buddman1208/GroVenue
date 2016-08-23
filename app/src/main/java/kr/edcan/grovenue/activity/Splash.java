7ea9a542b98dfee89800e88f5e107ef8691ca36b
Junseok Oh <kotohana5706@edcan.kr>
2016-08-23 20:12:52 +0900

    Splash: layout complete

--- app/src/main/java/kr/edcan/grovenue/activity/Splash.java
+++ app/src/main/java/kr/edcan/grovenue/activity/Splash.java
@@ -1,5 +1,7 @@
 package kr.edcan.grovenue.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,11 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Main.class));
+            }
+        }, 1000);
     }
 }
